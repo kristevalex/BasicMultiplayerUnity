45feb4e755f655bf2c74c76df588f8af528801b5
Alex <kristevalex@gmail.com>
2021-07-08 12:21:18 +0300

    Rotation of player

--- MultiplayerTest/Assets/Scripts/HelloWorldPlayer.cs
+++ MultiplayerTest/Assets/Scripts/HelloWorldPlayer.cs
@@ -10,8 +10,16 @@ public class HelloWorldPlayer : NetworkBehaviour
     float minMoveMouseDistThreshhold;
     [SerializeField]
     float maxVelocity;
+    [SerializeField]
+    float maxRotationVelocity;
 
     public NetworkVariableVector3 Position = new NetworkVariableVector3(new NetworkVariableSettings
+    {
+        WritePermission = NetworkVariablePermission.ServerOnly,
+        ReadPermission = NetworkVariablePermission.Everyone
+    });
+
+    public NetworkVariableVector3 Direction = new NetworkVariableVector3(new NetworkVariableSettings
     {
         WritePermission = NetworkVariablePermission.ServerOnly,
         ReadPermission = NetworkVariablePermission.Everyone
@@ -29,26 +37,22 @@ public class HelloWorldPlayer : NetworkBehaviour
 
     void MoveInDirection(Vector3 direction)
     {
+        SubmitMovementInDirectionRequestServerRpc(direction);
         if (NetworkManager.Singleton.IsServer)
-        {
-            transform.position += direction.normalized * maxVelocity;
-            Position.Value = transform.position;
-        }
-        else
-        {
-            SubmitMovementInDirectionRequestServerRpc(direction);
-        }
+            transform.position = Position.Value;
     }
 
     [ServerRpc]
-    void SubmitMovementInDirectionRequestServerRpc(Vector3 direction, ServerRpcParams rpcParams = default)
+    void SubmitMovementInDirectionRequestServerRpc(Vector3 targetDirection, ServerRpcParams rpcParams = default)
     {
-        Position.Value += direction.normalized * maxVelocity;
+        Direction.Value = Vector3.RotateTowards(Direction.Value, targetDirection, maxRotationVelocity, float.PositiveInfinity);
+        Position.Value += Direction.Value.normalized * maxVelocity;
     }
 
     void Update()
     {
         transform.position = Position.Value;
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, Direction.Value);
     }
 
     void FixedUpdate()
